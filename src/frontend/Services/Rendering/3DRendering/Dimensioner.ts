5428089c154449c7a5742c9c6daced74612e9b5a
WebAxol <webaxol@gmail.com>
2023-05-28 17:08:52 -0600

    I have been adapting the sub-service 'Dimensioner' at /3DRendering; it will use the scene-model of the 'Camera' instance to calculate the required information to render each pixel column. It might be necessary to change or add some things to the agent-type 'Ray', possibly store the total distance travelled from source to collision, as we won't traverse the ray-tree to calculate these values recursively  at rendering time.

--- src/frontend/Services/Rendering/3DRendering/Dimensioner.ts
+++ src/frontend/Services/Rendering/3DRendering/Dimensioner.ts
@@ -1,5 +1,7 @@
 import Service from "../../Service.js";
 import { Camera, camera } from "../../../utils/Camera.js";
+import { SceneChunk } from "../../../utils/SceneModel.js";
+import { Ray } from "../../../setUp/agentTypes.js";
 
 class Dimensioner extends Service{
 
@@ -12,10 +14,19 @@ class Dimensioner extends Service{
         this.interpolations = 0;
     }
 
-
     public executeAsSubordinate(){
  
-        // Too many variables and constants; a clousure is used to avoid declaring them over and over each call: Memory saved
+        const sceneModel = camera.sceneModel;
+
+        for(let l = sceneModel.layers.length - 1; l >= 0; l--){
+
+            sceneModel[l].forEach(sceneChunk => {
+                this.calculateChunkDimensions(sceneChunk);
+            });
+        }
+    }
+
+    public calculateChunkDimensions(sceneChunk :SceneChunk, prevDistance = 0, adjustment = undefined){
 
         const cameraDegree = camera.rays[Math.floor((camera.rays.length) / 2)].degree;
         const context = this.chief.context;
@@ -23,22 +34,14 @@ class Dimensioner extends Service{
         const netTotal = bruteTotal * (this.interpolations + 1); 
         const canvasWidth  = this.chief.canvas.width;
         const canvasHeight = this.chief.canvas.height;
-
-        // We call 'fraction' to every individual rectangle computed, and we call 'sceneChuck' to every group of fractions that include two rectangles in between which interpolation has occurred
-        // We'll store the information of pre-calculated fractions so that when the data from the adjacent ray is calculated, interpolation can be performed to compute sceneChunks
-
         const fractionQueue = [];
 
-        var index :number;
+        let from :Ray = sceneChunk.from;
+        let to   :Ray = sceneChunk.to;
 
-        for(index = 0; index < camera.rays.length; index++) _calculateDimensions(this,camera.rays[index]);
-     
-        // Each fraction is a portion of the bruteTotal length of the canvas horizontally
+        function calculateRayDetails(ray, prevDistance :number = 0, adjustment :number = 1){
 
-        function _calculateDimensions(service, ray, prevDistance = 0, adjustment : any = undefined){
-
-            if(!ray.collidesWith) return;
-
+            let index     = Math.floor(ray.degree / camera.rays[camera.rays.length - 1].degree) * camera.rays.length;
             let distanceX = Math.abs(Math.abs(ray.collidesAt.x) - Math.abs(ray.source.pos ? ray.source.pos.x : ray.source.collidesAt.x));
             let distanceY = Math.abs(Math.abs(ray.collidesAt.y) - Math.abs(ray.source.pos ? ray.source.pos.y : ray.source.collidesAt.y));
             let distance  = Math.hypot(distanceX,distanceY) + prevDistance;
@@ -48,33 +51,31 @@ class Dimensioner extends Service{
             adjustment = adjustment || Math.cos(Math.abs((Math.abs(cameraDegree - ray.degree) / 180) * Math.PI));
             let adjustedDistance = distance * adjustment;
 
-            let leftTop  = { x : ((canvasWidth * index) / bruteTotal), y : (canvasHeight / 2) - (canvasHeight) / adjustedDistance};
-            let size     = { x : canvasWidth / netTotal, y : (canvasHeight * 2) / adjustedDistance};
-
-            // Create sceneChunck to render
-            // TODO : implement interpolation algorithm to split sceneChunck into more pixel columns
-
-            let sceneChunck = {
-
-                // Spatial and dimensional information
-
-                leftTop  : leftTop,
-                size     : size,
-                distance : distance,
-
-                // Information of item with which ray collided
-
-                point    : ray.collidesAt,
-                item     : ray.collidesWith
-            }
-            
-            if(sceneChunck.item.opacity < 1 && ray.reflected.getType){
-                _calculateDimensions(service,ray.reflected,distance, adjustment);
+            let rayDetails = {
+                leftTop : { x : ((canvasWidth * index) / bruteTotal), y : (canvasHeight / 2) - (canvasHeight) / adjustedDistance},
+                size    : { x : canvasWidth / netTotal, y : (canvasHeight * 2) / adjustedDistance }
             }
 
-            service.chief._onvariablesCalculated(sceneChunck); // Internal event notified to chief module
+            return rayDetails;
         }
+
+        sceneChunk.details = {
+
+            initial : {
+                leftTop : {}
+            },
+            change : {
+                leftTop : {}
+            },
+
+            columns : NaN
+        };
     }
+
+    public interpolateDetails(detailA,detailB){
+        
+    }
+
 }
 
 export default Dimensioner;
