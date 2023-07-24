89dccfcb378bb9055cfcf097f153fcbc688e270c
WebAxol <webaxol@gmail.com>
2023-07-24 12:30:07 -0600

    I had been playing with deformating effects that occur as a result of an error while calculating the distance between an object in negative coords and one with positive coordinates. This might be a bug, but it could potentially be used to create interesting transitions between one scenario to another one. This deformation only manifests when transitioning from positive to negative coordinates, so it would not affect the visuals of elements in a same quarter of 2d space.

--- src/frontend/Services/Rendering/3DRendering/Orchestrator.ts
+++ src/frontend/Services/Rendering/3DRendering/Orchestrator.ts
@@ -35,11 +35,14 @@ class  SceneRenderer3D extends Service{
         // Render ceiling
 
         this.context.fillStyle = 'rgba(0,0,0,1)';
+        this.context.fillStyle = 'white';
+
         this.context.fillRect(0,0,this.canvas.width,this.canvas.height);
 
         // Render floor
     
         this.context.fillStyle = '#0e020e';
+        this.context.fillStyle = 'white';
         this.context.fillRect(0,0,this.canvas.width,this.canvas.height / 2);
 
 
