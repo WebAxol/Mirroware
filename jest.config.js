5ca3415e10177db7da730203df0c1a7fe551e92b
WebAxol <webaxol@gmail.com>
2023-12-27 10:42:41 -0600

    Jest for typescript has been successfully configured; we had to install jest-types and add 'jest' to the tsconfig.json 'types[]' array. Furthermore, I decided that the tests folder will be '__tests__' at 'src' but won't be transpiled as the directory is excluded at tsconfig.json.

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+export default {
+    "preset" : 'ts-jest',
+    "testEnvironment" : 'node',
+    "moduleFileExtensions" : ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    "testRegex" : '(/__tests__/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
+};
