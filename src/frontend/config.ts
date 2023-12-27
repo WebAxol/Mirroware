5babbb789ac5f2bb52e362dce84ae5513f2665e5
WebAxol <webaxol@gmail.com>
2023-12-26 19:25:33 -0600

    I modified the 'darkness' attribute of the config.ts configuration json renaming it to 'fog' replacing its occurrences along the project: the logic is intact. I also created a new property called 'fogColor' at config.ts and replaced the hard-coded '0,0,0' black background color at the RGBA service with that dynamic property to experiment with different background color tones. Finally, the /tests folder that contained useless material was cleaned: I will start testing some of the main services and utilities of the project for code security.

--- src/frontend/config.ts
+++ src/frontend/config.ts
@@ -1,7 +1,8 @@
 const CONFIG = {
 
     resolution : 300,
-    darkness   : true,
+    fog        : true,
+    fogColor   : "10,0,0", // RGB
     blurEffect : 0,
     waveEffect : true,
     lightLevel : 6
