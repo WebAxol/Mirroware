14c4dcc281b404896add2e3a7e53a7708a651157
WebAxol <webaxol@gmail.com>
2023-12-28 20:08:04 -0600

    The tests will be produced and executed at the 'test' branch, because I had to change typescript and babel configuration to make jest compatible with typescript and resolve modules. Therefore, I got rid of jest.config.js and the __tests__ folder at this branch (dev). From now forward, testing will be isolated from development - until I find an optimal workflow for testing and developing.

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-export default {
-    "preset" : 'ts-jest',
-    "testEnvironment" : 'node',
-    "moduleFileExtensions" : ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    "testRegex" : '(/__tests__/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
-};
