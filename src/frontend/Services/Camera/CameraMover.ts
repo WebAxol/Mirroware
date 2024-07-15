a1de326bf19d4316f1480df903668de67d40192a
WebAxol <webaxol@gmail.com>
2024-07-15 12:35:27 -0600

    Free camera movement is now a feature thanks to the adapted 'CameraMover' module, which responds to the user keyboard events.

--- src/frontend/Services/Camera/CameraMover.ts
+++ src/frontend/Services/Camera/CameraMover.ts
@@ -1,6 +1,6 @@
-import Service from '../Service.js';
+import Service            from '../Service.js';
 import { Camera, camera } from '../../utils/scene/Camera.js';
-import Vector2D from '../../utils/physics/Vector2D.js';
+import Vector2D           from '../../utils/physics/Vector2D.js';
 
 class CameraMover extends Service{
 
@@ -9,21 +9,21 @@ class CameraMover extends Service{
     private control;
     private command_KeyMap;
     private keyPressCount :number = 0;
-    private speed :number = 0.31;
+    private speed :number = 0;
 
     constructor(){
         super();
 
         this.dt = 0;
-        /*
+        
         this.command_KeyMap = {
-            w : () => { this.translateCamera(0, 1) },
-            s : () => { this.translateCamera(0,-1) },
-            a : () => { this.translateCamera( 1,0) },
-            d : () => { this.translateCamera(-1,0) },
-            k : () => { this.rotateCamera(-3 * this.dt) },
-            ñ : () => { this.rotateCamera( 3 * this.dt) },
-        }*/
+            w : () => { this.translateCamera(1, 0) },
+            s : () => { this.translateCamera(-1,0) },
+            a : () => { this.translateCamera( 0,1) },
+            d : () => { this.translateCamera(0,-1) },
+            k : () => { this.rotateCamera(  30 * this.dt) },
+            l : () => { this.rotateCamera( -30 * this.dt) },
+        }
 
         this.control = {
             w : false,
@@ -31,7 +31,7 @@ class CameraMover extends Service{
             d : false,
             a : false,
             k : false,
-            ñ : false
+            l : false
         }
     }
 
@@ -39,7 +39,7 @@ class CameraMover extends Service{
 
         this.dt = 50 / this.world.fps;
 
-        this.speed = 0.31 * this.dt;
+        this.speed = 0.09 * this.dt;
         
         let keys = Object.keys(this.control);
 
@@ -50,34 +50,37 @@ class CameraMover extends Service{
             }
         });
     }
-    /*
+    
     private rotateCamera(angle :number = 0){
 
-        camera.rays.forEach(ray => {
-            ray.degree = (ray.degree + angle) % 360;
-            if(ray.degree < 0) ray.degree += 360;
-        });
+
+        if(!camera || !camera.castEdge || !camera.castCenter) return false;
+        
+        const complex = [ Math.cos(angle / Math.PI / 180), Math.sin(angle / Math.PI / 180) ];
+        
+        camera.castCenter.direction.complexRotate(complex);
+        camera.castEdge.direction.complexRotate(complex);
+
     };
+    
+    private translateCamera(forward :number = 0, lateral : number = 0){
 
-    private translateCamera(x :number = 0, y : number = 0){
+        if(!camera || !camera.castEdge || !camera.castCenter) return false;
 
-        let midRay = camera.rays[Math.floor(camera.rays.length / 2)];
+        const complex = [ Math.cos(Math.PI / 2), Math.sin(Math.PI / 2) ];
 
-        let sense  = (midRay.degree > 270 || midRay.degree < 90)   ?  1 : -1;
-        
-        let direction = y ? midRay.slope : -1 * Math.pow(midRay.slope,-1);
-        let normalize = Vector2D.normalize(new Vector2D(1,direction));
+        let frontalMovement   = Vector2D.scale(camera.castCenter.direction, forward);
+        let lateralMovement   = Vector2D.complexRotate(camera.castCenter.direction, complex).scale(lateral);
+        let movementDirection = frontalMovement.add(lateralMovement).normalize();
+        let displacement      = movementDirection.scale(this.speed);
 
-        let addX :number = (normalize.x * sense * (x | y) * this.speed);
-        let addY :number = (normalize.y * sense * (x | y) * this.speed);
-        
-        if(Math.abs(addX) >= 0) camera.pos.x += addX;
-        if(Math.abs(addY) >= 0) camera.pos.y += addY;
+        console.log(displacement,this.speed);
+        camera.pos.add(displacement);
 
     }
 
     // EVENTS
-
+    
     public onkeydown(info){
         if(this.control[info.key] !== undefined && this.control[info.key] !== true ){
             this.control[info.key] = true;
@@ -89,7 +92,6 @@ class CameraMover extends Service{
             this.control[info.key] = false;
         }
     }
-    */
 }
 
 export default CameraMover;
